478f6bbbe0ca555dc8a2fc7843425e649e21deb3
Asmita-das2502 <hiralald327@gmail.com>
2024-08-01 19:59:01 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,10 @@ app.use(express.json({ limit: "13kb" }));
 app.use(express.urlencoded({ extended: true, limit: "13kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+//routes import
 
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 export { app };
